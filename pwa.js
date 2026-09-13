43111952d6cc5ab220d0cb5c1de96ad142b73b51
Hootson <ingebretson.eric@gmail.com>
2026-09-13 15:01:31 -0600

    Release V4.5.30

--- pwa.js
+++ pwa.js
@@ -1,4 +1,4 @@
-const SIDELINE_STATS_VERSION="4.5.29";
+const SIDELINE_STATS_VERSION="4.5.30";
 
 document.title=`Sideline Stats V${SIDELINE_STATS_VERSION}`;
 window.addEventListener("DOMContentLoaded",()=>{
