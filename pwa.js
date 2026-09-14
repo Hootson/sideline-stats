f179500f7a0faf51a6078283b2b637bc745f63bc
Hootson <ingebretson.eric@gmail.com>
2026-09-13 21:42:06 -0600

    Bump Sideline Stats to V4.5.32

--- pwa.js
+++ pwa.js
@@ -1,4 +1,4 @@
-const SIDELINE_STATS_VERSION="4.5.31";
+const SIDELINE_STATS_VERSION="4.5.32";
 
 document.title=`Sideline Stats V${SIDELINE_STATS_VERSION}`;
 window.addEventListener("DOMContentLoaded",()=>{
