afef0387ac694a10a9ae57aea9d17188fed10910
Hootson <ingebretson.eric@gmail.com>
2026-09-16 13:32:22 -0600

    Refresh cache for V4.5.38 viewer analytics

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='sideline-stats-v4-5-37-voice-save-scroll';
+const CACHE='sideline-stats-v4-5-38-viewer-analytics';
 const ASSETS=['./','./index.html','./styles.css','./voice-followup.css','./followup-loader.js','./app.js','./field-position.js','./field-orientation.js','./cloud-conflict.js','./game-lifecycle.js','./voice-workflow.js','./edit-play-model.js','./voice-play.js','./coach-analytics.js','./commercial-access.js','./pwa.js','./brand-header.png','./brand-field.png','./icon.png','./snap-tracker.html','./snap-tracker.js','./parent-viewer.html','./parent-viewer.js'];
 
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
