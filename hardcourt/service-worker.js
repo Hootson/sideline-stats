9554297a2667355f41df8322f92efbcb6f588e06
Hootson <ingebretson.eric@gmail.com>
2026-09-21 16:19:47 -0600

    Bump Hardcourt UI cache to ui51

--- hardcourt/service-worker.js
+++ hardcourt/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="hardcourt-ui50-20260921";
-const ASSETS=["./","./index.html","./styles.css?v=ui50","./app.js?v=ui50","./engine.js","./manifest.webmanifest?v=ui50","./court-iphone.png","./court-ipad.png","./court-analytics.png"];
+const CACHE="hardcourt-ui51-20260921";
+const ASSETS=["./","./index.html","./styles.css?v=ui51","./app.js?v=ui51","./engine.js","./manifest.webmanifest?v=ui51","./court-iphone.png","./court-ipad.png","./court-analytics.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(async c=>{for(const asset of ASSETS){try{const r=await fetch(asset,{cache:"reload"});if(r.ok)await c.put(asset,r.clone())}catch(_){}}}).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x.startsWith("hardcourt-")&&x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener("message",e=>{if(e.data==="SKIP_WAITING")self.skipWaiting()});
