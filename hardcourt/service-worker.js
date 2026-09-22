7b5894998e9da6b40b24aad898181562436407ae
Hootson <ingebretson.eric@gmail.com>
2026-09-22 11:29:10 -0600

    UI70: keep full basketball visible in responsive headers

--- hardcourt/service-worker.js
+++ hardcourt/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="hardcourt-ui69-20260922";
-const ASSETS=["./","./index.html","./styles.css?v=ui53","./app.js?v=20260922-ui69","./engine.js","./manifest.webmanifest?v=ui53","./court-iphone.png","./court-ipad.png","./court-analytics.png","./hardcourt-header-arena.jpg?v=ui69"];
+const CACHE="hardcourt-ui70-20260922";
+const ASSETS=["./","./index.html","./styles.css?v=ui53","./app.js?v=20260922-ui70","./engine.js","./manifest.webmanifest?v=ui53","./court-iphone.png","./court-ipad.png","./court-analytics.png","./hardcourt-header-arena.jpg?v=ui70"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(async c=>{for(const asset of ASSETS){try{const r=await fetch(asset,{cache:"reload"});if(r.ok)await c.put(asset,r.clone())}catch(_){}}}).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x.startsWith("hardcourt-")&&x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener("message",e=>{if(e.data==="SKIP_WAITING")self.skipWaiting()});
