b8a69912c9ce4c2e373dbee7a87afcba5654f98d
Hootson <ingebretson.eric@gmail.com>
2026-09-24 21:55:29 -0600

    Cache Hardcourt onboarding in account preview

--- hardcourt-account-preview/service-worker.js
+++ hardcourt-account-preview/service-worker.js
@@ -1,6 +1,6 @@
-const CACHE="hardcourt-account-preview-20260924b";
+const CACHE="hardcourt-account-preview-20260924c";
 const CACHE_PREFIX="hardcourt-account-preview-";
-const ASSETS=["./","./index.html","./styles.css?v=20260920-ui53","./app.js?v=20260924-account2","./legacy-app.js","./engine.js","./account-flow.js","./account-integration.js","./cloud-account.js","./commercial-config.js","./account-flow.css","./manifest.webmanifest?v=ui53","./court-iphone.png","./court-ipad.png","./court-analytics.png","./hardcourt-header-arena-ui71.jpg?v=ui71"];
+const ASSETS=["./","./index.html","./styles.css?v=20260920-ui53","./app.js?v=20260924-account3","./onboarding.js?v=20260924-account3","./legacy-app.js","./engine.js","./account-flow.js","./account-integration.js","./cloud-account.js","./commercial-config.js","./account-flow.css","./manifest.webmanifest?v=ui53","./court-iphone.png","./court-ipad.png","./court-analytics.png","./hardcourt-header-arena-ui71.jpg?v=ui71"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(async c=>{for(const asset of ASSETS){try{const r=await fetch(asset,{cache:"reload"});if(r.ok)await c.put(asset,r.clone())}catch(_){}}}).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x.startsWith(CACHE_PREFIX)&&x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener("message",e=>{if(e.data==="SKIP_WAITING")self.skipWaiting()});
